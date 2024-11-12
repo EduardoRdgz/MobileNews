bd05ddb534e71938fe1cfc2e13d702b938d1c938
Dan R Guzman <dr7rodriguez9319@gmail.com>
2024-11-11 18:27:22 -0600

    Setup navigation controller and created fragment container for activity

--- app/src/main/java/com/example/mobilenews/ui/Home/HomeActivity.kt
+++ app/src/main/java/com/example/mobilenews/ui/Home/HomeActivity.kt
@@ -2,16 +2,40 @@ package com.example.mobilenews.ui.Home
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupActionBarWithNavController
 import com.example.mobilenews.databinding.ActivityMainBinding
+import com.example.mobilenews.R
 
 class HomeActivity: AppCompatActivity() {
 
     private lateinit var binding:   ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        super.onCreate(savedInstanceState)
+        setSupportActionBar(binding.toolbar)
+        setupNavigation()
+    }
+
+
+    /*
+     * Setup the navigation controller
+     */
+    private fun setupNavigation(){
+        val navController = findNavController(R.id.fragment_container)
+        setupActionBarWithNavController(navController)
+    }
+
+    /*
+     * Handle the back button
+     */
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = findNavController(R.id.fragment_container)
+        return navController.navigateUp() || super.onSupportNavigateUp()
+
     }
 
 }
